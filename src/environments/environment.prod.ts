f48d63c934d536cfdcd83936de19e8b4649d77ae
Marcos Vogado <marcoshcvogado@gmail.com>
2025-11-19 13:06:33 -0300

    introducing project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
 export const environment = {
   production: true
 };
+
+export const SpotifyConfiguration = {
+  clientId: '779594abfed7429a82c7f3b3bcd55532',
+  authEndpoint: 'https://accounts.spotify.com/authorize',
+  redirectUrl: 'https//localhost:4200/login/',
+  scopes: [
+    "user-read-currently-playing", // musica tocando agora.
+    "user-read-recently-played", // ler musicas tocadas recentemente
+    "user-read-playback-state", // ler estado do player do usuario
+    "user-top-read", // top artistas e musicas do usuario
+    "user-modify-playback-state", // alterar do player do usuario.
+    "user-library-read", // ler biblioteca dos usuarios
+    "playlist-read-private", // ler playlists privads
+    "playlist-read-collaborative" // ler playlists colaborativas
+  ]
+}
